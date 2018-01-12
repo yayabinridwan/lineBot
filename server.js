8a4dbf50f8288578eaf87d961d35f3f39fe14fee
Dion Aditya <dionaditya@student.undip.ac.id>
2018-01-12 20:34:15 +0700

    my second commit

---
+++ server.js
@@ -0,0 +1,111 @@
+'use strict';
+
+const line = require('@line/bot-sdk');
+const express = require('express');
+const axios = require('axios');
+const cp = require('child-process-promise');
+const config = {
+  channelAccessToken: "bP51u0Q7bORxv5Eh4Z3kP4SoDLEqP+ysPnqxCRy9oZwjsSoQHTviSLk7BVbapb0PFXhkniJpz9wAqGpDp+4J2MhuZmrmZWCizhSBXllUKuonnx81ESGGkB8CDJiwqIk64DK4E1V6+nqePqEQSWpRagdB04t89/1O/w1cDnyilFU=" ,
+  channelSecret: "056b3f3a932f10d7f86011989907f0ac",
+};
+const DDG = require('node-ddg-api').DDG;
+const ddg = new DDG('botLine');
+ 
+
+// create LINE SDK client
+const client = new line.Client(config);
+
+
+
+
+// need raw buffer for signature validation
+
+// init with auth
+
+const app = express();
+ 
+// register a webhook handler with middleware
+// about the middleware, please refer to doc
+app.post('/callback', line.middleware(config), (req, res) => {
+  Promise
+    .all(req.body.events.map(handleEvent))
+    .then((result) => res.json(result))
+    console.log(JSON.stringify(req.body))
+ 
+});
+
+const replyText = (token, texts) => {
+  texts = Array.isArray(texts) ? texts : [texts];
+  return client.replyMessage(
+    token,
+    texts.map((text) => ({ type: 'text', text }))
+  );
+};
+
+ 
+function handleText(message, replyToken, source) {
+ 
+  
+  switch (message.text) {
+    case 'hai':
+      if (source.userId) {
+        return client.getProfile(source.userId)
+          .then((profile) => replyText(
+            replyToken,
+            [
+              `Halo kak ${profile.displayName}`,
+              `gimana kabarnya?`,
+            ]
+          ));
+      } else {
+        return replyText(replyToken, 'Bot can\'t use profile API without user ID');    
+        }
+      default:
+        var search = ddg.instantAnswer(message.text, {skip_disambig: '0'}, function(err, response) {
+            console.log(response);
+              })
+        
+
+         console.log(search);
+         return replyText(replyToken, message.text);
+  }
+}
+// listen on port
+function handleEvent(event) {
+  switch (event.type) {
+    case 'message':
+      const message = event.message;
+      switch (message.type) {
+        case 'text':
+          return handleText(message, event.replyToken, event.source);
+        default:
+          throw new Error(`Unknown message: ${JSON.stringify(message)}`);
+      }
+
+    case 'follow':
+      return replyText(event.replyToken, 'Got followed event');
+
+    case 'unfollow':
+      return console.log(`Unfollowed this bot: ${JSON.stringify(event)}`);
+
+    case 'postback':
+      let data = event.postback.data;
+      if (data === 'DATE' || data === 'TIME' || data === 'DATETIME') {
+        data += `(${JSON.stringify(event.postback.params)})`;
+      }
+      return replyText(event.replyToken, `Got postback: ${data}`);
+
+    case 'beacon':
+      return replyText(event.replyToken, `Got beacon: ${event.beacon.hwid}`);
+
+    default:
+      throw new Error(`Unknown event: ${JSON.stringify(event)}`);
+  }
+}
+
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`listening on ${port}`);
+
+    });
